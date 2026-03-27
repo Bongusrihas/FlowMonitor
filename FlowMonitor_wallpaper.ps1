32f950a98e8857a266a5aa2a268262e064711d4b
Bongusrihas <srihasbongu1105@gmail.com>
2026-03-27 17:48:14 +0530

    Added static wallpaper feature

---
+++ FlowMonitor_wallpaper.ps1
@@ -0,0 +1,20 @@
+$wallpaperPath = "$PSScriptRoot\logs\image.jpg"
+
+Add-Type @"
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+
+while ($true) {
+    if (Test-Path $wallpaperPath) {
+        [Wallpaper]::SystemParametersInfo(20, 0, $wallpaperPath, 3)
+        RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+    } else {
+        Write-Host "Image not found: $wallpaperPath"
+    }
+
+    Start-Sleep -Seconds 600
+}
